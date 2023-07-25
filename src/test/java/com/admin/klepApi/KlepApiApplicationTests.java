d82fbdc30da254fd0441e56060bd2e10baf25fae
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-07-25 19:22:41 -0300

    initial commit

---
+++ src/test/java/com/admin/klepApi/KlepApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin.klepApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlepApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
