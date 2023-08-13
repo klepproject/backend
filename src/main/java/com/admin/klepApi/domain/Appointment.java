230d19669d3367ed1fd58f99fd5f48ced8fbd313
Adler <adler.alves@hotmail.com>
2023-08-13 20:42:06 -0300

    FEAT: instanciando variaveis da classe Appointment

--- src/main/java/com/admin/klepApi/domain/Appointment.java
+++ src/main/java/com/admin/klepApi/domain/Appointment.java
@@ -1,4 +1,29 @@
 package com.admin.klepApi.domain;
 
-public class Appointment {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+public class Appointment implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    @ManyToOne
+    @JoinColumn(name = "professional_id")
+    private Professional professional;
+
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+    private LocalDateTime date;
 }
