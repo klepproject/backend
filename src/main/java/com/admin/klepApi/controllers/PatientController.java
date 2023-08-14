0c3118597ca582e74122703787b781abae0b3709
Adler <adler.alves@hotmail.com>
2023-08-14 20:19:32 -0300

    FEAT: setting patient controller

--- src/main/java/com/admin/klepApi/controllers/PatientController.java
+++ src/main/java/com/admin/klepApi/controllers/PatientController.java
@@ -1,9 +1,40 @@
 package com.admin.klepApi.controllers;
 
+import com.admin.klepApi.domain.Patient;
+import com.admin.klepApi.services.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/patient")
 public class PatientController {
+
+    @Autowired
+    PatientService patientService;
+
+    @GetMapping
+    public ResponseEntity<List<Patient>> findAll(){
+        List<Patient> patientList = patientService.findAll();
+
+        return ResponseEntity.ok().body(patientList);
+    }
+
+    @GetMapping
+    public ResponseEntity<Patient> findById(@PathVariable Long patientId) {
+        Optional<Patient> patient = patientService.findById(patientId);
+
+        return patient.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    public ResponseEntity<Patient> findByPhone() {
+
+    }
+
 }
--- src/main/java/com/admin/klepApi/services/PatientService.java
+++ src/main/java/com/admin/klepApi/services/PatientService.java
@@ -1,7 +1,12 @@
 package com.admin.klepApi.services;
 
+import com.admin.klepApi.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PatientService {
+
+    @Autowired
+    PatientRepository patientRepository;
 }
