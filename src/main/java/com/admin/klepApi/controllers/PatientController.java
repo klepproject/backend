d7f22f292a9842a2d44610f81d42f78276cff2e1
Adler <adler.alves@hotmail.com>
2023-08-14 19:54:05 -0300

    FEAT: controller do paciente

---
+++ src/main/java/com/admin/klepApi/controllers/PatientController.java
@@ -0,0 +1,9 @@
+package com.admin.klepApi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/patient")
+public class PatientController {
+}
