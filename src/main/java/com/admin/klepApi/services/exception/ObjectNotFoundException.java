c9840c6dbaa314485f331bcf7d9f41eff1276c5a
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-08-31 16:33:03 -0300

    FEAT: added error handling

---
+++ src/main/java/com/admin/klepApi/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.admin.klepApi.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+
+    public ObjectNotFoundException(String msg, Throwable cause){ super(msg, cause); }
+}
